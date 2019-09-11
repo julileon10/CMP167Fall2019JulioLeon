2f26f1531f10ea4efc96849994e201240187a6d9
julileon10 <54653734+julileon10@users.noreply.github.com>
2019-09-11 12:35:15 -0400

    Add files via upload

---
+++ week3.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+/**
+ * @author Student
+ *
+ */
+public class week3 {
+		public static void main(String [] args) {
+//			System.out.println(78+98); // answer: 176
+//			System.out.println('a' + 'b'); //195
+//			System.out.println('a' - 'b'); //-1
+//			System.out.println('0' + '0'); //96
+//			System.out.println('a' + 1); //98
+//			System.out.println('a'); //97 //
+			
+			int num = 18;
+			double dollars = 0.1111001;
+			
+			String name1 = "Julio";
+			String name2 = "Ronaldy";
+			
+			System.out.printf(" I miss my %d girlfriends, ", num); 
+			System.out.printf(" Ronaldo has $%.2f in his card", dollars);
+			
+			System.out.printf(" and %s is %d years older than %s", name1, num, name2);
+		
+		}
+
+}
