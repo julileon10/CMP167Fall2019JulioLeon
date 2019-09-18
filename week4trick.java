b44b2a54e3e775b087280ad5d4996e6c5e942b24
julileon10 <54653734+julileon10@users.noreply.github.com>
2019-09-18 12:36:47 -0400

    Add files via upload

---
+++ week4trick.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class week4trick {
+	public static void isDivisible(int num1, int num2) {
+		
+		if( num1 % num2 == 0) {
+			System.out.printf("%d is divisible by %d", num1, num2);
+		}else {
+			System.out.printf("%d is not divisible by %d", num1, num2);
+		}
+		
+	}
+
+}
