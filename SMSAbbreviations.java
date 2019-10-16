ec2bd74dcce5ae9b34f01470c8e405c82a383d6a
julileon10 <54653734+julileon10@users.noreply.github.com>
2019-10-16 11:42:18 -0400

    Add files via upload

---
+++ SMSAbbreviations.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+public class SMSAbbreviations {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter SMS Abbreviation: ");
+		String abbreviation = input.nextLine();
+		
+		abbreviation = abbreviation.toUpperCase();
+		
+		String translation = "";
+		
+		
+		if (abbreviation.equals("LOL")) {
+			translation = "Laughing Out Loud";
+		}
+		else if (abbreviation.equals("TMI")) {
+			translation = "Too Much Information";
+		}
+		else if (abbreviation.equals("SMH")) {
+			translation = "Shaking My Head";
+		}
+		else {
+			System.out.println("Unkown Abbreviation");
+		}
+		
+		System.out.println(translation);
+		
+		
+		
+		
+	}
+}
